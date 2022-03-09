b8dc9daae26b18daced7ecd9329f370113410adc
willidu <95777710+willidu@users.noreply.github.com>
2022-03-09 17:47:55 +0100

    fiks oppgave 3b

--- Oblig 1/oppgave3.py
+++ Oblig 1/oppgave3.py
@@ -1,10 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(-2, 2, 21)
-y = x.copy()
-vx, vy = np.meshgrid(np.cos(x)*np.sin(y), np.sin(x)*np.cos(y))
+t = np.linspace(-1, 1, 6)
+x, y = np.meshgrid(t, t)
+vx, vy = np.cos(x)*np.sin(y), -np.sin(x)*np.cos(y)
 
-plt.quiver(x, y, vx, -vy)
+plt.quiver(x, y, vx, vy)
 plt.axis('equal')
 plt.show()
